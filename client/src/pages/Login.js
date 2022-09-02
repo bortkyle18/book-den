e4f9cd9df73b51d1f2e6e611800ea7a3ea161496
Caleb Barkman <calebbarkman57@gmail.com>
2022-09-02 14:16:59 -0400

    update

--- client/src/pages/Login.js
+++ client/src/pages/Login.js
@@ -1,76 +1,89 @@
-import { useState } from "react"
-import Cookie from "js-cookie"
-import { Alert, Button, Container, Form } from 'react-bootstrap'
+import React, { useState } from 'react';
+import { Form, Button, Alert } from 'react-bootstrap';
 
-const LoginPage = (props) => {
-  const [ loginCreds, setLoginCreds ] = useState({ email: "", password: "" })
-  const [ formMessage, setFormMessage ] = useState({ type: "", msg: "" })
+import Auth from '../utils/auth';
+import { useMutation } from '@apollo/react-hooks';
+import { LOGIN_USER } from '../utils/mutations';
 
-  /*
-    TODO:
-    - set the form fields so that they get their value from the loginCreds in state
-    - write code so that typing into the form fields updates the correct value in state 
-    - write code so that when the submit button is clicked, the credentials are submitted 
-      to the api
-    - write code to parse the response sent back from the api, and show a message on 
-      the page that says whether the login succeeded or failed
-  */
+const LoginForm = () => {
+  const [userFormData, setUserFormData] = useState({ email: '', password: '' });
+  const [validated] = useState(false);
+  const [showAlert, setShowAlert] = useState(false);
+  const [loginUser] = useMutation(LOGIN_USER);
 
-  const handleLogin = async (e) => {
-    e.preventDefault()
-    setFormMessage({ type: "", msg: "" })
-    const authCheck = await fetch("/api/user/auth", {
-      method: "POST",
-      headers: { "Content-Type": "application/json" },
-      body: JSON.stringify(loginCreds)
-    })
-    const authResult = await authCheck.json()
+  const handleInputChange = (event) => {
+    const { name, value } = event.target;
+    setUserFormData({ ...userFormData, [name]: value });
+  };
 
-    // If the login was good, save the returned token as a cookie
-    if( authResult.result === "success" ){
-      Cookie.set("auth-token", authResult.token)
-      setFormMessage({ type: "success", msg: "Your login was successful. Proceed!" })
-    } else {
-      setFormMessage({ type: "danger", msg: "We could not log you in with the credentials provided." })
+  const handleFormSubmit = async (event) => {
+    event.preventDefault();
+
+    
+    const form = event.currentTarget;
+    if (form.checkValidity() === false) {
+      event.preventDefault();
+      event.stopPropagation();
     }
-    setLoginCreds({ email: "", password: "" })
-  }
+
+    try {
+      const { data } = await loginUser({
+        variables: {...userFormData} 
+      });
+
+      Auth.login(data.login.token);
+    } catch (err) {
+      console.error(err);
+      setShowAlert(true);
+    }
+
+    setUserFormData({
+      username: '',
+      email: '',
+      password: '',
+    });
+  };
 
   return (
-    <Container style={{ padding: "50px 200px"}}>
-      <Form onSubmit={handleLogin}>
-        <Form.Group className="mb-3" controlId="email">
-          <Form.Label>Email address</Form.Label>
-          <Form.Control 
-            type="email" 
-            name="email"
-            placeholder="Enter email" 
-            value={ loginCreds.email }
-            onChange={ (e) => setLoginCreds({ ...loginCreds, [e.target.name]: e.target.value })}
-          />
-        </Form.Group>
-
-        <Form.Group className="mb-3" controlId="password">
-          <Form.Label>Password</Form.Label>
-          <Form.Control 
-            type="password" 
-            name="password"
-            placeholder="Password" 
-            value={ loginCreds.password }
-            onChange={ (e) => setLoginCreds({ ...loginCreds, [e.target.name]: e.target.value })}
-          />
-        </Form.Group>
-
-        <Button variant="primary" type="submit">Submit</Button>
-      </Form>
-      
-      { formMessage.msg.length > 0 && (
-        <Alert variant={formMessage.type} style={{ marginTop: "2em" }}>
-          { formMessage.msg }
+    <>
+      <Form noValidate validated={validated} onSubmit={handleFormSubmit}>
+        <Alert dismissible onClose={() => setShowAlert(false)} show={showAlert} variant='danger'>
+          Something went wrong with your login credentials!
         </Alert>
-      )}
-    </Container>
-  )
-}
+        <Form.Group>
+          <Form.Label htmlFor='email'>Email</Form.Label>
+          <Form.Control
+            type='text'
+            placeholder='Your email'
+            name='email'
+            onChange={handleInputChange}
+            value={userFormData.email}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Email is required!</Form.Control.Feedback>
+        </Form.Group>
 
-export default LoginPage
+        <Form.Group>
+          <Form.Label htmlFor='password'>Password</Form.Label>
+          <Form.Control
+            type='password'
+            placeholder='Your password'
+            name='password'
+            onChange={handleInputChange}
+            value={userFormData.password}
+            required
+          />
+          <Form.Control.Feedback type='invalid'>Password is required!</Form.Control.Feedback>
+        </Form.Group>
+        <Button
+          disabled={!(userFormData.email && userFormData.password)}
+          type='submit'
+          variant='success'>
+          Submit
+        </Button>
+      </Form>
+    </>
+  );
+};
+
+export default LoginForm;
